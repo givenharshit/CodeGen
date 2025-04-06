d04c52fc87696f89385d232fefff86f979f29847
Harshit Bawiela <97221844+givenharshit@users.noreply.github.com>
2025-04-06 18:08:13 +0530

    added methods for cors in index.js

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const app = express(); // appHandler
 app.use(
     cors({
       origin: process.env.CORS_ORIGIN || "*",
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
       credentials: true,
     })
   );
